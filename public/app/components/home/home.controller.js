b7a77de350dc75e312da5ebcd535268d04966c06
Arya Solhi <aryasolhi@gmail.com>
2016-06-04 22:37:28 -0400

    Added another stage to getting started and started working og on the manage deck view

--- public/app/components/home/home.controller.js
+++ public/app/components/home/home.controller.js
@@ -5,25 +5,43 @@
         .module('flasht')
         .controller('HomeController', homeController);
 
-    homeController.$inject = ["$log", "Auth", "UserData", "DeckData"];
+    homeController.$inject = ["$log", "Auth", "UserData", "DeckData", "$firebaseArray"];
 
-    function homeController($log, Auth, UserData, DeckData) {
+    function homeController($log, Auth, UserData, DeckData, $firebaseArray) {
         var vm = this;
         Auth.diff(vm);
         UserData.getUserData(vm);
+        DeckData.getDecks(vm);
 
         vm.signin = Auth.signin;
         vm.createDeck = createDeck;
-        vm.noDecks = noDecks;
+        vm.addCard = addCard;
+        vm.numDecks = numDecks;
+        vm.selectDeck = selectDeck;
 
-        function noDecks() {
-            return (!!vm.user && vm.userdata);
+        function numDecks(n) {
+            return (!!vm.user && vm.userdata && vm.decks && (vm.decks.length == n));
         }
 
-        function createDeck() {
-            DeckData.pushDeck({
-                name: vm.newDeck
-            });
+        function createDeck($event) {
+            if (($event.type == 'keypress' && $event.keyCode == 13) || ($event.type == 'click')) {
+                vm.decks.$add({
+                    name: vm.newDeck
+                }).then(function () {
+                    vm.selectedDeck = vm.decks[vm.decks.length - 1];
+                });
+                vm.newDeck = "";
+            }
+        }
+
+        function addCard($event) {
+            if (($event.type == 'keypress' && $event.keyCode == 13) || ($event.type == 'click')) {
+
+            }
+        }
+
+        function selectDeck(id) {
+            vm.selectedDeck = vm.decks.$getRecord(id);
         }
     }
 
